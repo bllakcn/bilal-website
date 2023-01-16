ac562e78f8765c6336287b46f384f8cd7f2cf549
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-01-16 15:29:58 +0100

    work on Navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+
   let y: number;
   let scrolled: boolean = false;
 
@@ -14,21 +15,31 @@
 
 <svelte:window bind:scrollY={y}/>
 <aside class:scrolled class="text-stone-400 fixed right-0 text-right p-5 pr-3 hidden lg:block">
-  <ul class="list-disc list-inside">
+  <ul class="mr-5 text-xs leading-loose tracking-wider font-medium">
     <li><a href="#about">About</a></li>
     <li><a href="#toolset">Toolset</a></li>
     <li><a href="#projects">Projects</a></li>
     <li><a href="#contact">Contact</a></li>
   </ul>
+  {#if scrolled}
+  <ul class="py-2 mr-5 text-xs leading-loose tracking-wider font-medium">
+    <li><a class="" href="/">Go top</a></li>
+  </ul>  
+  {/if}
 </aside>
 
 <style>
   aside {
     transition: all .5s;
+
     direction: rtl;
   }
   aside.scrolled{
     opacity: .5;
+    padding: .5rem auto;
+  }
+  aside.scrolled ul {
+    line-height: 1;
   }
   aside.scrolled:hover {
     opacity: 1;
@@ -36,6 +47,7 @@
   aside li {
     position: relative;
     transition: all .1s;
+    transition: line-height .3s;
   }
   aside li:hover {
     color: #fff;
