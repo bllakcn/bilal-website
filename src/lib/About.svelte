13a6336efb2211f1fce53b9b17e877458818b9a9
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-01-17 12:57:12 +0100

    working on sections

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,3 +1,61 @@
-<section id='about' class="min-h-screen">
-  <h3>About me</h3>
+<section class='relative bg-black text-neutral-300 flex flex-col justify-center items-center pb-16'>
+  <div class='absolute md:hidden bg-neutral-800 w-11/12 h-60 left-5 top-2 -rotate-3'></div>
+  <h2 class='z-10 text-center text-neutral-200 text-xl my-10'><span class='highlight-about relative z-10 text-2xl font-black'>About</span> Me</h2>
+  <div class='z-10 text-center w-11/12 mx-auto max-w-lg'>
+    <p><b>Architecture</b> graduate freelancing in <b>tech</b>. Passionate about <b>designing</b> and <b>developing</b> that great <b class='underline decoration-wavy'>user experience</b>.</p>
+  </div>
+  <!-- <Link className='z-10 mt-5 underline-btn' to='/projects'>Take a look at the projects</Link> -->
 </section>
+
+<style>
+  @keyframes hoverUnderline {
+  0%{
+    width: 0%;
+  }
+  100%{
+    width: 200px;
+  }
+}
+
+.highlight-about::before{
+  content: '';
+  width: 70px;
+  height: 20px;
+  left: -5px;
+  bottom: -4px;
+  background-color:rgba(49, 46, 129,.5);
+  position: absolute;
+  z-index: -1;
+}
+.underline{
+  text-decoration-skip-ink: none;
+  text-decoration-thickness: .1em;
+  text-decoration-color: rgba(6, 182, 212,.9);
+}
+.underline-btn {
+  position: relative;
+  font-weight: 500;
+}
+.underline-btn::after {
+  content: '';
+  position: absolute;
+  width: 2px;
+  height: 1px;
+  background-color: rgb(212 212 212);
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%);
+  transition: all 250ms;
+}
+.underline-btn:hover::after {
+  content: '';
+  position: absolute;
+  width: 200px;
+  height: 1px;
+  background-color: rgb(212 212 212);
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%);
+  animation: hoverUnderline 200ms;
+}
+</style>
--- src/lib/Toolset.svelte
+++ src/lib/Toolset.svelte
@@ -1,3 +1,73 @@
-<section id="toolset">
-  <h3>My Toolset</h3>
+<script>
+  import reactlogo from './assets/react.png'
+  import sveltelogo from './assets/svelte.png'
+  import vuelogo from './assets/vue.png'
+  import htmllogo from './assets/html.png'
+  import csslogo from './assets/css.png'
+  import jslogo from './assets/js.png'
+  import tailwindlogo from './assets/tailwind.png'
+  import pslogo from './assets/ps.png'
+  import ailogo from './assets/ai.png'
+  import lrlogo from './assets/lr.png'
+  import xdlogo from './assets/xd.png'
+  import splinelogo from './assets/spline.png'
+  import sasslogo from './assets/sass.png'
+
+  const frontSkills = [
+  {name:htmllogo, id:1},
+  {name:csslogo, id:2},
+  {name:jslogo, id:3},
+  {name:reactlogo, id:4},
+  {name:sveltelogo, id:5},
+  {name:vuelogo, id:6},
+  {name:sasslogo, id:7},
+  {name:tailwindlogo, id:8}
+]
+const contentSkills = [
+  {name:pslogo, id:1},
+  {name:ailogo, id:2},
+  {name:xdlogo, id:3},
+  {name:lrlogo, id:4},
+  {name:splinelogo, id:5}
+]
+</script>
+
+<section class='bg-black text-neutral-300 flex flex-col justify-center pb-16'>
+  <h2 class='text-center text-neutral-200 text-xl my-10'>My <span class='highlight-tools relative z-10 text-2xl font-black'>Toolset</span></h2>
+  <div class='mx-auto text-center'>
+    <p class='font-thin mx-6 text-sm tracking-widest'>Front-end development</p>
+    <div class='grid ml-0 p-4 grid-cols-8 gap-5 grey-filtered'>
+      {#each frontSkills as frontSkill (frontSkill.id)}
+        <img class="w-12"src={frontSkill.name} alt={frontSkill.name}>
+      {/each}
+    </div>
+  </div>
+  <div class='mx-auto text-center'>
+    <p class='font-thin mx-6 text-sm tracking-widest'>Content Creation</p>
+    <div class='grid ml-0 p-4 grid-cols-5 gap-5 grey-filtered'>
+      {#each contentSkills as contentSkill (contentSkill.id)}
+      <img class="w-12"src={contentSkill.name} alt={contentSkill.name}>
+    {/each}
+    </div>
+  </div>
 </section>
+
+<style>
+  .grey-filtered img{
+  filter: grayscale(95%);
+  transition: filter .2s ease-out;
+}
+.grey-filtered img:hover{
+  filter: grayscale(0);
+}
+.highlight-tools::before{
+  content: '';
+  width: 100px;
+  height: 20px;
+  left: -5px;
+  bottom: -4px;
+  background-color: rgba(49, 46, 129,.5);
+  position: absolute;
+  z-index: -1;
+}
+</style>
